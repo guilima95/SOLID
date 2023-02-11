890f571a7e3463b7cdfba889f23c41c071b22b86
Guilherme Lima <guilhermelima@neowrk.com>
2023-02-10 23:28:26 -0300

    fix: adding param score in after solution without inheract

--- SOLID.OCP/Before/Book.cs
+++ SOLID.OCP/Before/Book.cs
@@ -2,16 +2,16 @@ namespace SOLID.OCP.Before;
 
 public class Book
 {
-    public void Review(string bookName, int pages, TypeBook typeBook)
+    public void Review(string bookName, int pages, int score, TypeBook typeBook)
     {
         if (typeBook is TypeBook.Digital)
         {
-            // to do
+            // to do review based on score
         }
 
         if (typeBook is TypeBook.Physical)
         {
-            // to do
+            // to do review based on score
         }
     }
 }
